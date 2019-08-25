aef293157d44ab2ca6c6f341f34bcf03ff8832db
Bueno <zymbabueno@gmail.com>
2019-08-24 21:44:29 -0300

    Load models and user registration test

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World woo" });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jon 2',
+    email: 'mail2@example.com',
+    password_hash: '87638764',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
