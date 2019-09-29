d98d67b121974409fe418f2e840a78f7531364df
Bueno <zymbabueno@gmail.com>
2019-09-28 21:16:44 -0300

    Setup email with nodemailer. Send email to provider when appointment is canceled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '44089269ea507a',
+    pass: '38fdfea62e98f1',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
