ac94f38630c7a2487e2b81e98af6601947653dbd
Bueno <zymbabueno@gmail.com>
2019-10-04 22:06:01 -0300

    Virtual fields in appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -21,7 +21,7 @@ class AppointmentController {
         canceled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 2,
       offset: (page - 1) * 2,
       include: [
