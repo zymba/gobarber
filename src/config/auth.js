8446dcaef5c1276f0050e5b7495bfc3fa4ebb6f2
Bueno <zymbabueno@gmail.com>
2019-10-06 19:11:32 -0300

    Configuring environment variables

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '870fee1ca6c36660677957ded75dc663',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://60155f97d24c4e589fb4d67b253c43c8@sentry.io/1771399',
+  dsn: process.env.SENTRY_DSN,
 };
