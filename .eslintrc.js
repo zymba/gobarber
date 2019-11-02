606356e6f997aa7683648ad33be6ea7c5dec5583
Bueno <zymbabueno@gmail.com>
2019-11-02 20:35:48 -0300

    Update gitignore and eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,9 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
